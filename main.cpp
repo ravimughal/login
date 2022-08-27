195bc0089e4863bf6e2721dfa2b2c77189167a49
Ravi Mughal <ravimughal@hotmail.com>
2022-08-27 17:02:24 -0300

    criando user e senha

--- main.cpp
+++ main.cpp
@@ -3,13 +3,31 @@ using namespace std;
 
 void cadastrar()
 {
-    if (FILE * fd = fopen("contas.csv", "r"))
+    if (FILE *fd = fopen("contas.csv", "r")) // se existe "contas.csv" não criará um arquivo encima do já existente
     {
         cout << "arquivo existente";
-    } else {
-        ofstream contas("contas.csv");
+        ofstream contas;
+        contas.open("contas.csv", ios_base::app);
+        string user, password;
+        cout << "Digite o usuário";
+        cin >> user;
+        cout << "Digite a senha";
+        cin >> password;
+        contas << user << "," << password << endl;
+        contas.close();
     }
-
+    else
+    {
+        ofstream contas("contas.csv");
+        string user, password;
+        cout << "Digite o usuário";
+        cin >> user;
+        cout << "Digite a senha";
+        cin >> password;
+        contas << "user" << "," << "password" << endl;
+        contas << user << "," << password << endl;
+        contas.close();
+    }
 }
 
 int main()
@@ -17,9 +35,12 @@ int main()
     string conta;
     cout << "Possui conta? (s/n)";
     cin >> conta;
-    if (conta == "s" or conta == "sim") {
+    if (conta == "s" or conta == "sim")
+    {
         cout << "hi";
-    } else {
+    }
+    else
+    {
         cadastrar();
     }
     return 0;
